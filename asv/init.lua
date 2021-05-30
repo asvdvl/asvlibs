6f4b554ec55d58fa2077e45f31dafda49b56217e
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-05-30 21:58:10 +0300

    Added list download(is lib not found) Added startup message Added feature for get library via calling(reqire("asv")("sublib"))

--- asv/init.lua
+++ asv/init.lua
@@ -1,12 +1,46 @@
+--TODO
+--[]download libs
+--[]update libs
+--[]can store the file until the reboot
+--[v]no load all
 --vars
 local var = {...}
-local this = {}
+local this = {_NOTE = "This table not empty! libraries are loading by accessing their name, via table.library"}
+local internet = require("internet")
 
---set metatable
-local function mtIndex(table, key)
+--settings
+local st = {
+    shortUrl = "https://raw.githubusercontent.com/asvdvl/asvlibs/master/asv/",
+    listURL = "https://raw.githubusercontent.com/asvdvl/asvlibs/master/downloadList.txt",
+    downloadIfNotExist = true,
+    startupMessage = true
+}
+
+if st.startupMessage then
+    io.stdout:write("asv libs init for automatic download and update libs\n")
+    io.stdout:write("if you want to disable this message, change the startupMessage variable in the settings table of the asv/init.lua file\n")
+end
+
+local function downloadFile(package)
+    local handle = internet.request(st.listURL)
+    local result = ""
+    for chunk in handle do
+        result = result..chunk
+    end
+
+end
+
+local function getLibrary(table, key)
+    local path = package.searchpath(var[1].."."..key, package.path)
+    if not path then
+        downloadFile(var[1])
+    end
+
     table[key] = require(var[1].."."..key)
     return table[key]
 end
-this = setmetatable({}, {__index = mtIndex})
+
+--set metatable
+this = setmetatable({}, {__index = getLibrary, __call = getLibrary})
 
 return this
