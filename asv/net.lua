fc035d67f556b5169aac6e75660847ee2baaa71e
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-07-01 23:54:32 +0300

    Added send function Added broadcast via specific interface function Updated documentation

--- asv/net.lua
+++ asv/net.lua
@@ -5,10 +5,20 @@ this.l2 = {}
 local port = 1 --constant
 
 
-function this.l2.broadcast(...)
+function this.l2.broadcastViaAll(...)
     for addr in pairs(cmp.list("modem")) do
         cmp.proxy(addr).broadcast(port, ...)
     end
+end
+
+function this.l2.broadcast(srcAddr, ...)
+    checkArg(1, srcAddr, "string")
+    cmp.proxy(cmp.get(srcAddr)).broadcast(port, ...)
+end
+
+function this.l2.send(srcAddr, dctAddr, ...)
+    checkArg(1, srcAddr, "string")
+    cmp.proxy(cmp.get(srcAddr)).send(dctAddr, port, ...)
 end
 
 return this
