40b12d0afebe2ea075e32f55821a440c104e091e
asvdev <57688070+asvdvl@users.noreply.github.com>
2021-07-05 01:03:40 +0300

    Added opening port on component_added event

--- asv/net.lua
+++ asv/net.lua
@@ -1,11 +1,29 @@
 local cmp = require("component")
+local event = require("event")
 
 local this = {}
 this.l2 = {}
 local port = 1 --constant
 
+--Init
 for addr in pairs(cmp.list("modem")) do
     cmp.proxy(addr).open(port)
+end
+
+local function eventComponentAddedProcessing(_, addr, componentType)
+    if componentType == "modem" then
+        cmp.proxy(addr).open(port)
+    end
+end
+
+event.listen("component_added", eventComponentAddedProcessing)
+
+--L2
+local function getModemFromAddress(addr)
+    if addr then
+        return cmp.proxy(cmp.get(addr))
+    end
+    return cmp.modem
 end
 
 function this.l2.broadcastViaAll(...)
@@ -15,19 +33,13 @@ function this.l2.broadcastViaAll(...)
 end
 
 function this.l2.broadcast(srcAddr, ...)
-    checkArg(1, srcAddr, "string")
-    cmp.proxy(cmp.get(srcAddr)).broadcast(port, ...)
+    checkArg(1, srcAddr, "string", "nil")
+    getModemFromAddress(srcAddr).broadcast(port, ...)
 end
 
 function this.l2.send(srcAddr, dctAddr, ...)
     checkArg(1, srcAddr, "string", "nil")
-    local modem
-    if srcAddr then
-        modem = cmp.proxy(cmp.get(srcAddr))
-    else
-        modem = cmp.modem
-    end
-    modem.send(dctAddr, port, ...)
+    getModemFromAddress(srcAddr).send(dctAddr, port, ...)
 end
 
 return this
